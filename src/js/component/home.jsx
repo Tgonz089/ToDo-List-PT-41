88ad29a7b9a0773f0985507153bf842fa9856f53
Tgonz089 <tristangonzales8936@gmail.com>
2022-10-07 18:25:13 +0000

    fixed issue with PUT

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,53 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Button, Form } from "react-bootstrap";
 
-/*function GetList(){
-	
-	const requestOptions = {
-	method: 'GET',
-	redirect: 'follow'
-  };
-  
-  fetch("http://assets.breatheco.de/apis/fake/todos/user/Tristan", requestOptions)
-	.then(response => response.json())
-	.then(result => console.log(result))
-	.catch(error => console.log('error', error));
-}
-
-function PutList(){
-	fetch('http://assets.breatheco.de/apis/fake/todos/user/Tristan', {
-      method: "PUT",
-      body: JSON.stringify(todos),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(resp => {
-        console.log(resp.ok); // will be true if the response is successfull
-        console.log(resp.status); // the status code = 200 or code = 400 etc.
-        console.log(resp.text()); // will try return the exact result as string
-        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-    })
-    .then(data => {
-        //here is were your code should start after the fetch finishes
-
-        console.log(data); //this will print on the console the exact object received from the server
-    })
-    .catch(error => {
-        //error handling
-        console.log(error);
-    });
-}*/
-
 function TodoList() {
   const [todos, setTodos] = useState([{ text: "No tasks, add a task" }]);
 
-  useEffect(
-    () =>
-      fetch("https://assets.breatheco.de/apis/fake/todos/user/Tristan")
+  function getToDo (){
+  fetch("https://assets.breatheco.de/apis/fake/todos/user/Tristan")
         .then((response) => response.json())
         .then((data) => setTodos(data))
-        .catch((error) => console.log(error)),
+        .catch((error) => console.log(error))
+}
+  useEffect(
+    () =>
+      getToDo(),
     []
   );
 
@@ -61,9 +26,6 @@ function TodoList() {
         },
       })
         .then((resp) => {
-          console.log(resp.ok);
-          console.log(resp.status);
-          console.log(resp.text());
           return resp.json();
         })
         .then((data) => {
@@ -76,7 +38,7 @@ function TodoList() {
   }, [todos]);
 
   const addTodo = (text) => {
-    const newTodos = [...todos, { text }];
+    const newTodos = [...todos, { label:text,done:false }];
     setTodos(newTodos);
   };
 
@@ -126,7 +88,7 @@ function FormTodo({ add }) {
 function Todo({ todo, index, remove }) {
   return (
     <div className="todo">
-      <span>{todo.text}</span>{" "}
+      <span>{todo.label}</span>{" "}
       <Button className="Button" onClick={() => remove(index)}>
         ✕
       </Button>
